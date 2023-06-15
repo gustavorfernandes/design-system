2d9edc19e8b487da72d215d32fcf2d8d8b2b334b
Gustavo <gustavo.ribeiro.fernandes1@gmail.com.br>
2023-06-14 21:13:55 -0300

    chore: :wrench: more configs

--- packages/ui/typography/react/src/components/typography/typography.test.tsx
+++ packages/ui/typography/react/src/components/typography/typography.test.tsx
@@ -1,12 +1,10 @@
-import { describe, it, expect } from 'vitest'
-import { render, screen } from '@testing-library/react'
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
 
-import Typography from './Typography'
+import Typography from './Typography';
 
 describe('Typography', () => {
   it('should render the component', () => {
-    render(<Typography />)
-
-    expect(screen.getByText('Typography')).toBeInTheDocument()
-  })
-})
+    render(<Typography />);
+  });
+});
