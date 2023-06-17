151d06543af20dba68d6bcd8253471b83f049e39
Gustavo <gustavo.ribeiro.fernandes1@gmail.com.br>
2023-06-17 18:32:42 -0300

    feat: :zap: adding test and story files to typography component

--- packages/ui/typography/react/src/components/typography/typography.test.tsx
+++ packages/ui/typography/react/src/components/typography/typography.test.tsx
@@ -1,12 +1,38 @@
-import { describe, it, expect } from 'vitest'
 import { render, screen } from '@testing-library/react'
 
 import Typography from './Typography'
 
-describe('Typography', () => {
-  it('should render the component', () => {
-    render(<Typography />)
+describe('Typography component', () => {
+  test('renders children correctly', () => {
+    const text = 'Hello, World!'
+    render(<Typography>{text}</Typography>)
+    const typographyElement = screen.getByText(text)
+    expect(typographyElement).toBeInTheDocument()
+  })
 
-    expect(screen.getByText('Typography')).toBeInTheDocument()
+  test('applies correct variant class', () => {
+    const variant = 'h1'
+    render(<Typography variant={variant}>Heading 1</Typography>)
+    const typographyElement = screen.getByText('Heading 1')
+    expect(typographyElement).toHaveClass('typography__h1')
+  })
+
+  test('applies custom class names', () => {
+    const customClassName = 'custom-class'
+    render(<Typography className={customClassName}>Text</Typography>)
+    const typographyElement = screen.getByText('Text')
+    expect(typographyElement).toHaveClass(customClassName)
+  })
+
+  test('applies default "text" variant if no variant is provided', () => {
+    render(<Typography>Default variant</Typography>)
+    const typographyElement = screen.getByText('Default variant')
+    expect(typographyElement).toHaveClass('typography__text')
+  })
+
+  test('renders with the specified HTML element', () => {
+    render(<Typography as="h1">Heading 1</Typography>)
+    const headingElement = screen.getByRole('heading', { level: 1 })
+    expect(headingElement).toBeInTheDocument()
   })
 })
--- workshop/src/stories/typography.stories.tsx
+++ workshop/src/stories/typography.stories.tsx
@@ -1,3 +1,13 @@
-import { Typography as TypographyComponent } from 'typography'
+import { Typography as TypographyComponent } from 'typography';
 
-export const Typography = () => <TypographyComponent />
+export const Typography = () => (
+  <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+    <TypographyComponent variant="h1" as="h1">
+      Lorem Ipsum
+    </TypographyComponent>
+    <TypographyComponent variant="h2" as="h2">
+      Lorem Ipsum
+    </TypographyComponent>
+    <TypographyComponent as="p">Lorem Ipsum</TypographyComponent>
+  </div>
+);
